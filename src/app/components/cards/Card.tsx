2ab4ab85147509e60cb94be0919774f0d924654a
Worazme <39632157+Worazme@users.noreply.github.com>
2023-12-08 10:18:12 +0100

    img

--- src/app/components/cards/Card.tsx
+++ src/app/components/cards/Card.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from "react"
+import BackImg from '../../../../public/back.png'
 
-const Card = ({ children }: { children?: ReactNode }) => {
+const Card = ({ children, flipped }: { children?: ReactNode, flipped: boolean }) => {
   return (
     <div className="bg-blue-400 w-24 flex justify-center items-center aspect-[5/7] rounded-xl">
+      <div>
         <span className=" text-5xl">{children}</span>
+      </div>
+      <div className="rotate-180">
+        <span className=" text-5xl"></span>
+      </div>
     </div>
   )
 }
